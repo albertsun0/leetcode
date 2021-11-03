f733be6d94f2cb692cd64f235ae9f02d026b8c13
Albert Sun <albertsun0@gmail.com>
2021-11-03 17:39:13 -0500

    Update day1-5.js

--- dp study plan/day1-5.js
+++ dp study plan/day1-5.js
@@ -111,8 +111,8 @@ const robHelp = (nums, startIndex) => {
     return Math.max(dp[dp.length-1], dp[dp.length-2]);
 }
 // 3 2 2 3
-arr2 = [1,2,3];
-console.log(rob2(arr2, 0));
+// arr2 = [1,2,3];
+// console.log(rob2(arr2, 0));
 
 //TODO: make better solution
 
@@ -120,3 +120,131 @@ console.log(rob2(arr2, 0));
 //https://leetcode.com/problems/delete-and-earn/ 
 
 //
+
+const deleteAll = (nums) => {
+    let m = new Map();
+   // console.log(a.length);
+    max = 0;
+    nums.forEach(num => {
+        max = Math.max(num, max);
+        const total = m.get(num) || 0;
+        m.set(num, total + num); 
+    });
+    console.log(m);
+    dp = [];
+    dp[0] = m.get(0) || 0;
+    dp[1] = m.get(1) || 0;
+    
+    for(i = 2; i< max+1; i++){
+        c = m.get(i) || 0;
+        dp[i] = Math.max(dp[i-2] + c, dp[i-1]);
+    }
+    return Math.max(dp[dp.length-1], dp[dp.length-2]);
+}
+
+
+// let a = [3,4,2];
+
+// console.log(deleteAll(a));
+
+//DAY 4
+//jump game https://leetcode.com/problems/jump-game/
+
+//if all are >0 can reach end
+//can only stop if stuck at 0
+//if there is a 0, there must be atleast 
+
+var canJump = function(nums) {
+    if(nums[0] == 0 && nums.length != 1) return false;
+    maxDistance = [];
+    maxDistance[0] = nums[0];
+    for(i = 1; i< nums.length-1; i++){
+        maxDistance[i] = Math.max(maxDistance[i-1]-1, nums[i]);
+        if(maxDistance[i] <= 0){
+            return false;
+        }
+    }
+    console.log(maxDistance);
+    return true;
+};
+
+// let j = [3,2,1,0,4];
+// let k = [2,3,1,1,4];
+// let l = [2, 0, 0];
+// console.log(canJump(l));
+
+
+//Jump game 2 https://leetcode.com/problems/jump-game-ii/
+
+var jump = function(nums) {
+    let dp = [];
+    dp.length = nums.length;
+    dp.fill(10001);
+    dp[0] = 0;
+    for(i = 0; i< nums.length; i++){
+        for(j = i; j < i + nums[i]+1; j++){
+           // console.log(nums[i] + " " +j);
+            if(j < nums.length){
+                dp[j] = Math.min(dp[j] , dp[i]+1);
+               // console.log(i + "   " +dp + " " + dp[i]); 
+            }
+        }
+        if(dp[dp.length-1] !== 10001){return dp[dp.length-1];}
+    }
+    return dp[dp.length-1];
+};
+// let t = [1,1,1,1,1,1];
+// let n = [2,3,1,1,4];
+// let x = [2,3,0,1,4];
+// console.log(jump(x));
+// O(N) non dp solution?
+
+
+// maximum subarray https://leetcode.com/problems/maximum-subarray/ 
+
+var maxSubArray = function(nums) {
+    let max = -99999;
+    let cur = 0;
+    //atleast one positive, max subarray
+    //if all neg, ans is largest element
+    allNeg = true;
+    biggest = -99999;
+    for(i = 0; i<nums.length; i++){
+        cur += nums[i];
+        if(nums[i] > 0) allNeg = false;
+        if(nums[i] > biggest) biggest = nums[i];
+        if(cur < 0){
+            cur = 0;
+        }
+        if(cur > max){
+            max = cur;
+        }
+    }
+    if(!allNeg) return max;
+    return biggest;
+};
+
+var maxSubArrayDP = function(nums) {
+    let max = -99999;
+    let dp = [];
+    allNeg = true;
+    biggest = -99999;
+    dp[0] = nums[0];
+    for(i = 1; i<nums.length; i++){
+        dp[i] = nums[i] + dp[i-1];
+        if(nums[i] > 0) allNeg = false;
+        if(nums[i] > biggest) biggest = nums[i];
+        if(dp[i] < 0){
+            dp[i] = 0;
+        }
+        if(dp[i] > max){
+            max = dp[i];
+        }
+    }
+    if(!allNeg) return max;
+    return biggest;
+};
+
+let a = [-2,1,-3,4,-1,2,1,-5,4];
+// let b = [-1,-2,-1,-1,-1];
+console.log(maxSubArrayDP(a));
