bc398e7865d6c3ebebdc9a77d25e632382123c72
Albert Sun <albertsun0@gmail.com>
2021-11-10 16:53:04 -0600

    number spiral

--- cses/intro/6spiral.cpp
+++ cses/intro/6spiral.cpp
@@ -4,19 +4,23 @@ using namespace std;
 #define pb push_back
 #define ll unsigned long long
 //find value starting from mx^2,0
-int findx(ll start, ll max, ll x, ll y){
+void findx(ll start, ll max, ll x, ll y){
     if(y < max){
-        return start-y+1;
+        cout<<start-y+1;
     }
-    return (start-y+1)-(max-x);
+    else{
+        cout << (start-y+1)-(max-x);
+    }
 }
 
 //find value startinf from 0, mx^2
-int findy(ll start, ll max, ll x, ll y){
-    if(x < max){
-        return start-x+1;
+void findy(ll start, ll max, ll x, ll y){
+    if(x < max){  
+        cout<< start-x+1;
     }
-    return (start-y+1)-(max-y);
+    else{
+        cout<< (start-x+1)-(max-y);
+    }  
 }
 
 int main () {
@@ -29,15 +33,15 @@ int main () {
         ll m = max(x,y);
         
         ll t = m*m;
-        cout << m  << "\n";
-        cout << t  << "\n";
+        //cout << m  << "\n";
+        //cout << t  << "\n";
         if(t%2 == 0){
-            cout << findy(t,m,x,y) << "\n";
+            findy(t,m,x,y);
         }
         else{
-            cout << findx(t,m,x,y) << "\n";
+            findx(t,m,x,y);
         }
-        
+        cout << "\n";
     }
 
     return 0;
