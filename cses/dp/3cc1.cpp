e4d3ebb23ad9cca7a66e2265d46fcafe362f17cc
Albert Sun <albertsun0@gmail.com>
2021-11-16 13:15:18 -0600

    3

---
+++ cses/dp/3cc1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define pb push_back
+#define ll long long
+#define fio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+int main () {
+    fio;
+    int n; cin >> n;
+
+    return 0;
+}
+
+
+/*
+2 3 5 = 9
+//store how many ways you can get
+0 1 2 3 4 5 6 7 8 9
+1 0 1 0 1 0 1 0 1 0
+1 0 1 1 1 2 2 1 3 3   
+*/
