5b9eae22471f65c7804535614ab9509ae8dc8fff
Albert Sun <albertsun0@gmail.com>
2021-12-11 12:31:38 -0600

    2

---
+++ advent/4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define pb push_back
+#define ll long long
+#define fio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+int main () {
+    fio;
+    int n; 
+    int count = 0;
+    while(cin >> n){
+        count++;
+    }
+    cout << count;
+    return 0;
+}
