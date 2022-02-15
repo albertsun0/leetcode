ae3db159433f1ecb3c002857ee88565bac840aa1
qwertyuiop888 <albertsun0@gmail.com>
2022-02-14 21:42:52 -0600

    a

--- cses/graph/3buildroads.cpp
+++ cses/graph/3buildroads.cpp
@@ -9,18 +9,45 @@ using namespace std;
 int main () {
     fio;
     int n; cin >> n;
-    int cities[n+1];
-    for(int i = 0; i<n+1; i++){
-        cities[i] = 0;
-    }
-
-    for(int i = 0; i<n; i++){
+    vector<int> cities(n+1, -1);
+    int m; cin >> m;
+    for(int i = 0; i<m; i++){
         int a;
         int b;
         cin >> a >> b;
-
-        
-
+        if(cities[a] == -1 && cities[b] == -1){ //both nodes have not been seen yet
+            cities[a] = 0;  //0 means that it is the head node for the component
+            cities[b] = a; // any number means that node points to the head node of the component
+        }
+        else if(cities[a] == 0){
+            cities[b] = a;
+        }
+        else if(cities[b] == 0){
+            cities[a] == b;
+        }
+        else if(cities[a] > 0){
+            cities[b] = cities[a];
+        }
+        else if(cities[b] > 0){
+            cities[a] = cities[b];
+        }
     }
+    int count = 0;
+    vector<int> add;
+    for(int i = 1; i<=n; i++){
+        if(cities[i] == 0 || cities[i] == -1){
+            count++;
+            add.pb(i);
+        }
+    }
+    if(add.size() == 1){
+        cout << 0;
+        return 0;
+    }
+    cout << count - 1 << "\n";
+    for(int i = 0; i< add.size()-1; i++){
+        cout << add[i] << " " << add[i+1] << "\n";
+    }
+
     return 0;
 }
